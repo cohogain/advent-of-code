95539232eac2b845f5e4f4dd3e31c6da736c2eb7
cohogain <ciaran.ohogain@gmail.com>
2022-12-05 22:22:58 +0000

    removed test solution

--- 2022/day4/main.py
+++ 2022/day4/main.py
@@ -46,16 +46,3 @@ def p2(f):
         sequence_2.clear()
 
     return counter
-
-
-def p3(f):
-    ans = 0
-    for l in f:
-        a, b = l.strip().split(",")
-        a = set(irange(*map(int, a.split("-"))))
-        b = set(irange(*map(int, b.split("-"))))
-        if len(a & b) > 0:
-            ans += 1
-    return ans
-
-print(p3(f))
